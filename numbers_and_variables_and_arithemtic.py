9ebef16cc5c1b29acc45c5ee447506defca59561
Shiv Kr Bansal <shiv.bansal759@gmail.com>
2017-09-17 21:57:50 +0530

    Understanding numbers and arithematic operations

---
+++ numbers_and_variables_and_arithemtic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+a = 5
+b = 2
+print("a + b = %d"  % (a+b))
+print("a - b = " + str(a-b))
+
+print("a * b = %d" % (a*b))
+
+print("integer a / b  = %d" % (a//b))
+
+print("Float a/b  = %f"  % (float(a)/b))
+
+print("a power b = "  + str(a**b))
